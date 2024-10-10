c3110009a442c24c4ba2e8d02049d13bff751e68
philipgjreid <philipgjreid@yahoo.co.nz>
2024-10-10 18:57:40 +1300

    Changes to site formatting

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('draft_home/', views.home2, name="home2"),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, redirect
 
 def home(request):
 	return render(request, 'mainapp/home.html', {})
+
+def home2(request):
+	return render(request, 'mainapp/home2.html', {})
