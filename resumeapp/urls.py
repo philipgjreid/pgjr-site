47f3ac2f10a555e209f213715b9b229e58ff90a3
philipgjreid <philipgjreid@yahoo.co.nz>
2024-10-14 21:49:56 +1300

    Alternate resume page created

--- resumeapp/urls.py
+++ resumeapp/urls.py
@@ -5,4 +5,5 @@ from .import views
 urlpatterns = [
 	path('resume/', views.resume, name='resume'),
 	path('portfolio', views.portfolio, name='portfolio'),
+	path('resume2/', views.resume2, name='resume2'),
 ]
