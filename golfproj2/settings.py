16921aa72064a910d14a6f7049be11feb61de9f3
philipgjreid <philipgjreid@yahoo.co.nz>
2024-10-19 13:34:22 +1300

    Update static file settings

--- golfproj2/settings.py
+++ golfproj2/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_collected') # This worked to get PythonAnywhere to work.
 
 
